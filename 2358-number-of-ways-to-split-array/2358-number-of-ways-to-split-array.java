408b49d295674527a52baf4397ee635676dbb251
Aayisha Noora Jalaludeen <112081762+aayishajalal@users.noreply.github.com>
2025-01-03 20:15:41 +0530

    Time: 3 ms (59.69%), Space: 58.5 MB (60.21%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -1,19 +1,20 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long leftSum = 0, rightSum = 0;
-        for (int num : nums) {
-            rightSum += num;
+        //running sum --- intializing to 0 and use long to handle overflow
+        //rightSum = totalSum(arr) leftSum =0
+        //on the go --> leftSum+=arr[i] rightSum=-arr[i] check cond --> valid++
+        long leftSum=0, rightSum = 0; 
+        int validSplit =0;
+        for(int i:nums){
+            rightSum+=i;
         }
-
-        int validSplits = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
+        //dont compute for last element 
+        for(int i=0; i<nums.length-1; i++){
             leftSum += nums[i];
-            rightSum -= nums[i];
-            if (leftSum >= rightSum) {
-                validSplits++;
-            }
+            rightSum -=nums[i];
+            if(leftSum>=rightSum) validSplit++;
         }
-
-        return validSplits;
+           return validSplit;
     }
+ 
 }
