d4d566bc163b6458c9732cc2a55e664b39186b53
Aayisha Noora Jalaludeen <112081762+aayishajalal@users.noreply.github.com>
2025-02-02 09:19:01 +0530

    Time: 0 ms (100%), Space: 41.5 MB (27.27%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean check(int[] nums) {
+        //with circular checks 
         int violations =0;
-        //atmost 1 violation is allowed for rotated sorted array
-        for(int i=0; i<nums.length-1; i++){
-            //prev>next -- violations+=1
-            if(nums[i]>nums[i+1]) violations+=1;
-     
-        }
-               /*compare in circular manner - as rotated sorted array
-            last>first 
-            prev>next  --> violations+=1
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            //curr<prev
+            /*circular checks
+            compares 0 and n-1  --> 1 and 0 ---so on 
+                nums[0]
             */
-            if(nums[nums.length-1]>nums[0]) violations+=1;
-        //if violations is <=1 return true
-        return violations<=1;
+            if(nums[i] < nums[(i-1+n)%n]) violations+=1;
+            if(violations>1)return false;
+        }
+        return true;
     }
 }
