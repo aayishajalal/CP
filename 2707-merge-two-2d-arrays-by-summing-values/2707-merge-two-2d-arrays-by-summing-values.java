cd6f961dbd11c95caf708f66b79a03ade2cb68e7
Aayisha Noora Jalaludeen <112081762+aayishajalal@users.noreply.github.com>
2025-03-02 12:37:38 +0530

    Time: 1 ms (86.98%), Space: 45 MB (84.13%) - LeetHub

---
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        int maxLength = nums1.length + nums2.length;
+        int[][] res = new int[maxLength][2];
+
+        int i = 0, j = 0, k = 0;
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i][0] == nums2[j][0]) {
+                res[k][0] = nums1[i][0];
+                res[k][1] = nums1[i][1] + nums2[j][1];
+                i++; j++;
+            } else if (nums1[i][0] < nums2[j][0]) {
+                res[k][0] = nums1[i][0];
+                res[k][1] = nums1[i][1];
+                i++;
+            } else {
+                res[k][0] = nums2[j][0];
+                res[k][1] = nums2[j][1];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < nums1.length) {
+            res[k][0] = nums1[i][0];
+            res[k][1] = nums1[i][1];
+            i++; k++;
+        }
+
+        while (j < nums2.length) {
+            res[k][0] = nums2[j][0];
+            res[k][1] = nums2[j][1];
+            j++; k++;
+        }
+
+        return Arrays.copyOf(res, k);
+    }
+}
