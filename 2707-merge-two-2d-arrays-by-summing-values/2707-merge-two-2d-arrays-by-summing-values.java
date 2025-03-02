c9faee07a685827e14aa34eaa7e8aec1bf983fc7
Aayisha Noora Jalaludeen <112081762+aayishajalal@users.noreply.github.com>
2025-03-02 12:40:22 +0530

    Time: 1 ms (86.98%), Space: 45 MB (88.77%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,41 +1,51 @@
-import java.util.*;
-
 class Solution {
+
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        int maxLength = nums1.length + nums2.length;
-        int[][] res = new int[maxLength][2];
+        int N1 = nums1.length, N2 = nums2.length;
+        int ptr1 = 0, ptr2 = 0;
 
-        int i = 0, j = 0, k = 0;
-        
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i][0] == nums2[j][0]) {
-                res[k][0] = nums1[i][0];
-                res[k][1] = nums1[i][1] + nums2[j][1];
-                i++; j++;
-            } else if (nums1[i][0] < nums2[j][0]) {
-                res[k][0] = nums1[i][0];
-                res[k][1] = nums1[i][1];
-                i++;
+        List<int[]> mergedArray = new ArrayList<>();
+        while (ptr1 < N1 && ptr2 < N2) {
+            // If the id is same, add the values and insert to the result.
+            // Increment both pointers.
+            if (nums1[ptr1][0] == nums2[ptr2][0]) {
+                mergedArray.add(
+                    new int[] {
+                        nums1[ptr1][0],
+                        nums1[ptr1][1] + nums2[ptr2][1],
+                    }
+                );
+                ptr1++;
+                ptr2++;
+            } else if (nums1[ptr1][0] < nums2[ptr2][0]) {
+                // If the id in nums1 is smaller, add it to result and increment the pointer
+                mergedArray.add(nums1[ptr1]);
+                ptr1++;
             } else {
-                res[k][0] = nums2[j][0];
-                res[k][1] = nums2[j][1];
-                j++;
+                // If the id in nums2 is smaller, add it to result and increment the pointer
+                mergedArray.add(nums2[ptr2]);
+                ptr2++;
             }
-            k++;
         }
 
-        while (i < nums1.length) {
-            res[k][0] = nums1[i][0];
-            res[k][1] = nums1[i][1];
-            i++; k++;
+        // If pairs are remaining in the nums1, then add them to the result.
+        while (ptr1 < N1) {
+            mergedArray.add(nums1[ptr1]);
+            ptr1++;
         }
 
-        while (j < nums2.length) {
-            res[k][0] = nums2[j][0];
-            res[k][1] = nums2[j][1];
-            j++; k++;
+        // If pairs are remaining in the nums2, then add them to the result.
+        while (ptr2 < N2) {
+            mergedArray.add(nums2[ptr2]);
+            ptr2++;
         }
 
-        return Arrays.copyOf(res, k);
+        // Convert List<int[]> to int[][]
+        int[][] result = new int[mergedArray.size()][2];
+        for (int i = 0; i < mergedArray.size(); i++) {
+            result[i] = mergedArray.get(i);
+        }
+
+        return result;
     }
 }
