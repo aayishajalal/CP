0893544b195e6ccbadf1437fcbce3c9aa8da0cd4
Aayisha Noora Jalaludeen <112081762+aayishajalal@users.noreply.github.com>
2025-03-03 19:39:44 +0530

    Time: 0 ms (100%), Space: 42.7 MB (8.89%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,13 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count =0;
+        int index=0; 
         for(int i=0; i<nums.length; i++){
-            if(nums[i] == val){
-                nums[i]=Integer.MAX_VALUE;
-                count++;
-            } 
+            if(nums[i]!=val){
+                nums[index++]=nums[i];
+            }
         }
-        Arrays.sort(nums);
-        return nums.length-count;
+        return index;
     }
 }
