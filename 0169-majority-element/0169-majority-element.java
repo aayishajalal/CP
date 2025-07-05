e18d8c394b8fa60c7102876f0217faeccf99a7b4
Aayisha Noora Jalaludeen <112081762+aayishajalal@users.noreply.github.com>
2025-07-05 21:48:09 +0530

    Time: 5 ms (40.89%), Space: 54.2 MB (8.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-      int count = 0, elem = nums[0];
-      //1. Apply Moore's Voting Algorithm 
-      for(int i=0; i<nums.length; i++){
-        if(count==0){
-            elem=nums[i];
-        }
-        if(nums[i]==elem){
-            count++;
-        }else{
-            count--;
-        }
-      }
-      return elem;
-    //   //2. Verify the element appears more than n/2 times
-    //   int count1 = 0;
-    //   for(int i=0; i<nums.length; i++ ){
-    //     if(nums[i] == elem) count1++;
-    //   }
-    //   if(count1>nums.length/2) return elem;
-    //   return -1;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
