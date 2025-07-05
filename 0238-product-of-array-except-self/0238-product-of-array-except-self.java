75d2e638e96535e6085c65c32ff93567cf80641a
Aayisha Noora Jalaludeen <112081762+aayishajalal@users.noreply.github.com>
2025-07-05 22:34:39 +0530

    Time: 2 ms (88.53%), Space: 55.1 MB (94.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,27 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        //using pre and post arrays 
-        int n= nums.length;
-        int res[] = new int[n];
-        int pre[]  = new int[n];
-        int post[] = new int[n];
-        Arrays.fill(pre,1);
-        Arrays.fill(post,1);
+        //using pre and post variables fill with 1 
+        int pre =1, post =1;
+        int res[] = new int[nums.length];
+        //fill res[] with 1 
         Arrays.fill(res,1);
-        //pre array computation
-        pre[0] = 1;
-        for(int i=1; i<n; i++){
-            pre[i] = pre[i-1]*nums[i-1];
+        //fill pre --> inside res 
+        //run from 0 --> n
+        for(int i=0; i<nums.length; i++){
+            res[i]*=pre;
+            pre*=nums[i];
         }
-        //post array computation
-        for(int i=n-2; i>=0; i--){
-            post[i]=post[i+1]*nums[i+1];
+        //fill post --> inside res 
+        //run in n-1 --> 0
+        for(int i=nums.length-1; i>=0; i--){
+            res[i]*=post;
+            post*=nums[i];
         }
 
-        //res array computation --> pre[i]*post[i]
-        for(int i=0; i<n; i++){
-            res[i]=pre[i]*post[i];
-        }
         return res;
     }
 }
