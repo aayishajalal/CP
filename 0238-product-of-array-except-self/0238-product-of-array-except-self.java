61e443133fea34c839ec18fa786828b6ae2ac82c
Aayisha Noora Jalaludeen <112081762+aayishajalal@users.noreply.github.com>
2025-07-05 22:37:35 +0530

    Time: 2 ms (88.53%), Space: 55 MB (98.87%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         //using pre and post variables fill with 1 
-        int pre =1, post =1;
+        int post =1;
         int res[] = new int[nums.length];
-        //fill res[] with 1 
-        Arrays.fill(res,1);
         //fill pre --> inside res 
         //run from 0 --> n
-        for(int i=0; i<nums.length; i++){
-            res[i]*=pre;
-            pre*=nums[i];
+        res[0]=1;
+        for(int i=1; i<nums.length; i++){
+            res[i] = res[i - 1] * nums[i - 1];
         }
         //fill post --> inside res 
         //run in n-1 --> 0
